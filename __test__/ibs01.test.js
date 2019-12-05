aebe9178a88c5fc9342675601b41a55621bf54ac
Tim Chao <tim.chao@ingics.com>
2019-12-05 10:37:33 +0800

    add microsoft device type decoder

--- __test__/ibs01.test.js
+++ __test__/ibs01.test.js
@@ -79,7 +79,7 @@ describe('various ibs01 payload test', () => {
 });
 
 describe('real failure case set', () => {
-    it ('test #1', (done) => {
+    it ('test iRS02RG', (done) => {
         const message = '$GPRP,0C61CFC14B58,CC4B73906F8C,-21,02010612FF0D0083BC4D010000002400FCFE22074B58,1575440728';
         parser.parseMessage(message, (data) => {
             expect(data.parsedPayload.type).toBe('iRS02RG');
@@ -87,5 +87,13 @@ describe('real failure case set', () => {
             expect(data.parsedPayload.accel.z).toBe(-260);
             done();
         });    
+    })
+    it ('test windows 10 desktop', (done) => {
+        const message = '$GPRP,3D0A662DC412,C3674946C293,-62,1EFF0600010920025E294E5E30809130E56E2CA4701DC8EBED5396B6320B8F';
+        parser.parseMessage(message, (data) => {
+            expect(data.parsedPayload.mfg).toBe('Microsoft');
+            expect(data.parsedPayload.type).toBe('Windows 10 Desktop');
+            done();
+        });
     })
 });
