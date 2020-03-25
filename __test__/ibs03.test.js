07581e776bb5b6868b1203d9360b0bba56c37b97
Tim Chao <tim.chao@ingics.com>
2020-03-25 15:54:12 +0800

    feat(ibs03t): support new ibs03t, with humidity
    Signed-off-by: Tim Chao <tim.chao@ingics.com>

--- __test__/ibs03.test.js
+++ __test__/ibs03.test.js
@@ -4,12 +4,19 @@ describe('various ibs03 payload test', () => {
     it('iBS03T', (done) => {
         const message = '$GPRP,0C61CFC14A4E,E3C33FF55AEC,-50,02010612FF0D0083BC2801020A09FFFF000015030000';
         parser.parseMessage(message, (data) => {
-            console.log(data)
             expect(data.parsedPayload.type).toBe('iBS03T');
-            //expect(data.beacon).toBe('FE581D9DB308');
-            //expect(data.gateway).toBe('2F9203AFA66B');
-            //expect(data.parsedPayload.battery).toBe(310);
-            //expect(data.parsedPayload.events.button).toBe(true);
+            expect(data.parsedPayload.temperature).toBe(2314);
+            expect(typeof data.parsedPayload.humidity).toBe('undefined');
+            expect(typeof data.parsedPayload.events.button).not.toBe('undefined');
+            done();
+        });
+    })
+    it('iBS03T (NEW)', (done) => {
+        const message = '$GPRP,CDCB34E2D0A2,77AE1C1DC33D,-91,02010612FF0D0083BCAD0000A20B4700FFFF14000000';
+        parser.parseMessage(message, (data) => {
+            expect(data.parsedPayload.type).toBe('iBS03T');
+            expect(data.parsedPayload.temperature).toBe(2978);
+            expect(data.parsedPayload.humidity).toBe(71);
             done();
         });
     })
