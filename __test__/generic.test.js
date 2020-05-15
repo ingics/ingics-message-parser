9c8cfd08be6108f335e41f4a3d2d111ceead348f
Tim Chao <tim.chao@ingics.com>
2020-05-15 11:18:27 +0800

    feat: support generic temperature and humidity service data parser
    Signed-off-by: Tim Chao <tim.chao@ingics.com>

--- __test__/generic.test.js
+++ __test__/generic.test.js
@@ -17,15 +17,18 @@ describe('generic ad test', () => {
     it ('org.bluetooth.characteristic.temperature', () => {
         const payload = '0B09506978656C203420584C05166E2A1111'
         const advertisement = parser.parsePayload(payload)
-        expect(advertisement.serviceData[0].uuid).toBe('2A6E')
+        expect(advertisement.serviceData[0].uuid).toBe(0x2A6E)
         expect(advertisement.serviceData[0].data.readInt16LE()).toBe(4369)
+        expect(advertisement.temperature).toBe(43.69)
+        expect(advertisement.temperatureUnit).toBe('C')
     })
 
     it ('org.bluetooth.characteristic.humidity', () => {
         const payload = '0B09506978656C203420584C05166F2A1111'
         const advertisement = parser.parsePayload(payload)
-        expect(advertisement.serviceData[0].uuid).toBe('2A6F')
+        expect(advertisement.serviceData[0].uuid).toBe(0x2A6F)
         expect(advertisement.serviceData[0].data.readInt16LE()).toBe(4369)
+        expect(advertisement.humidity).toBe(43.69)
     })
 
     it ('16bit Service UUID', () => {
