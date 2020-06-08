77d115ddf69bee925ea77b2508a8bd7b192a451f
Tim Chao <tim.chao@ingics.com>
2020-06-08 17:55:34 +0800

    feat: support iGS03 GPSR (GNSS) message
    Signed-off-by: Tim Chao <tim.chao@ingics.com>

---
+++ __test__/igs03.test.js
@@ -0,0 +1,57 @@
+const parser = require('..')
+
+describe('special data for iGS03', () => {
+
+    it('GPS', () => {
+        const message = '$GPSR,C82B96AE3B04,C82B96AE3B04,-127,200608,073549,-24.993477,121.422981,0.0,3.5'
+        parser.parseMessage(message, (data) => {
+            const gnss = data.gnss
+            expect(gnss.latitude).toBe(-24.993477)
+            expect(gnss.longitude).toBe(121.422981)
+            expect(gnss.speed).toBe(0)
+            expect(gnss.hdop).toBe(3.5)
+            const fixDate = new Date(gnss.fixTimestamp)
+            expect(fixDate.getUTCFullYear()).toBe(2020)
+            expect(fixDate.getUTCMonth()).toBe(5)
+            expect(fixDate.getUTCDate()).toBe(8)
+            expect(fixDate.getUTCHours()).toBe(7)
+            expect(fixDate.getUTCMinutes()).toBe(35)
+        })
+    })
+
+    it('GPS, with timestamp', () => {
+        const message = '$GPSR,C82B96AE3B04,C82B96AE3B04,-127,200608,073549,-24.993477,121.422981,0.0,3.5,1591609841'
+        parser.parseMessage(message, (data) => {
+            expect(data.timestamp).toBe(1591609841000)
+            const gnss = data.gnss
+            expect(gnss.latitude).toBe(-24.993477)
+            expect(gnss.longitude).toBe(121.422981)
+            expect(gnss.speed).toBe(0)
+            expect(gnss.hdop).toBe(3.5)
+            const fixDate = new Date(gnss.fixTimestamp)
+            expect(fixDate.getUTCFullYear()).toBe(2020)
+            expect(fixDate.getUTCMonth()).toBe(5)
+            expect(fixDate.getUTCDate()).toBe(8)
+            expect(fixDate.getUTCHours()).toBe(7)
+            expect(fixDate.getUTCMinutes()).toBe(35)
+        })
+    })
+
+    it('GPS, with ms timestamp', () => {
+        const message = '$GPSR,C82B96AE3B04,C82B96AE3B04,-127,200608,073549,-24.993477,121.422981,0.0,3.5,1591609841.895'
+        parser.parseMessage(message, (data) => {
+            expect(data.timestamp).toBe(1591609841895)
+            const gnss = data.gnss
+            expect(gnss.latitude).toBe(-24.993477)
+            expect(gnss.longitude).toBe(121.422981)
+            expect(gnss.speed).toBe(0)
+            expect(gnss.hdop).toBe(3.5)
+            const fixDate = new Date(gnss.fixTimestamp)
+            expect(fixDate.getUTCFullYear()).toBe(2020)
+            expect(fixDate.getUTCMonth()).toBe(5)
+            expect(fixDate.getUTCDate()).toBe(8)
+            expect(fixDate.getUTCHours()).toBe(7)
+            expect(fixDate.getUTCMinutes()).toBe(35)
+        })
+    })
+})
