2513546074fd6dc8a107abe4a97698fa625ab3d2
Tim Chao <tim.chao@ingics.com>
2021-05-11 16:16:01 +0800

    feat: support trigger counter for iR2 and M2
    Signed-off-by: Tim Chao <tim.chao@ingics.com>

--- __test__/ibs02.test.js
+++ __test__/ibs02.test.js
@@ -7,7 +7,19 @@ describe('various ibs02 payload test', () => {
         parser.parseMessage(message, (data) => {
             const msd = data.advertisement.manufacturerData
             expect(msd.type).toBe('iBS02IR2')
+            expect(msd.counter).toBeUndefined()
             expect(msd.battery).toBe(2.88)
+            expect(msd.events.ir).toBe(true)
+        })
+    })
+
+    it('iBS02IR with counter', () => {
+        const message = '$GPRP,0081F9889BF9,F008D1789294,-42,02010612FF0D0083BC4D0120AAAA05000000020A0600'
+        parser.parseMessage(message, (data) => {
+            const msd = data.advertisement.manufacturerData
+            expect(msd.type).toBe('iBS02IR2')
+            expect(msd.battery).toBe(3.33)
+            expect(msd.counter).toBe(5)
             expect(msd.events.ir).toBe(true)
         })
     })
@@ -25,10 +37,19 @@ describe('various ibs02 payload test', () => {
     it('iBS02M2', () => {
         let msd = parser.parsePayload('02010612FF0D0083BC3E0140AAAAFFFF000004070000').manufacturerData
         expect(msd.type).toBe('iBS02M2')
+        expect(msd.counter).toBeUndefined()
         expect(msd.events.din).toBe(true)
-        expect(msd.events.button).toBe(undefined)
+        expect(msd.events.button).toBeUndefined()
         msd = parser.parsePayload('02010612FF0D0083BC3E0100AAAAFFFF000004070000').manufacturerData
+        expect(msd.counter).toBeUndefined()
         expect(msd.events.din).toBe(false)
-        expect(msd.events.hall).toBe(undefined)
+        expect(msd.events.hall).toBeUndefined()
+    })
+
+    it('iBS02M2 with Counter', () => {
+        let msd = parser.parsePayload('02010612FF0D0083BC240100AAAA37060000040B0600').manufacturerData
+        expect(msd.type).toBe('iBS02M2')
+        expect(msd.counter).toBe(1591)
+        expect(msd.events.din).toBe(false)
     })
 })
