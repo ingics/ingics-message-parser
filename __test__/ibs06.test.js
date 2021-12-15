cf220ae670c3530e34e5d1bf73793e7eb1f5764a
Tim Chao <tim.chao@ingics.com>
2021-12-15 16:36:04 +0800

    feat: support ibs06i
    Signed-off-by: Tim Chao <tim.chao@ingics.com>

--- __test__/ibs06.test.js
+++ __test__/ibs06.test.js
@@ -1,7 +1,7 @@
 const parser = require('..')
 
 describe('various ibs06 payload test', () => {
-    it('standard', () => {
+    it('ibs06', () => {
         const message = '$GPRP,1804ED7048F5,E3C33FF55AEC,-55,02010612FF2C0883BC4A0100AAAAFFFF000040110000'
         parser.parseMessage(message, (data) => {
             expect(data.beacon).toBe('1804ED7048F5')
@@ -9,6 +9,14 @@ describe('various ibs06 payload test', () => {
             const msd = data.advertisement.manufacturerData
             expect(msd.type).toBe('iBS06')
             expect(msd.battery).toBe(3.3)
+        })
+    })
+    it('ibs06i', () => {
+        const message = '$RSPR,EB8C79A8F138,F008D1789200,-89,12FF2C0883BC350100AAAAFFFF000036030000,1639557006.811'
+        parser.parseMessage(message, (data) => {
+            const msd = data.advertisement.manufacturerData
+            expect(msd.type).toBe('iBS06i')
+            expect(msd.battery).toBe(3.09)
         })
     })
 })
