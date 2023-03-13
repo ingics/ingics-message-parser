7e7a97b1706db2bf332f46253952e24a15bf13d0
Tim Chao <tim.chao@ingics.com>
2023-03-13 13:29:11 +0800

    feat: support iBS08 payload
    Signed-off-by: Tim Chao <tim.chao@ingics.com>

---
+++ __test__/ibs08.test.js
@@ -0,0 +1,16 @@
+const parser = require('..')
+
+describe('various ibs08 payload test', () => {
+
+    it('iBS03T', () => {
+        const message = '$GPRP,F83060BC466E,98F4AB891854,-82,02010612FF2C0883BC380140C0086608000048080400'
+        parser.parseMessage(message, (data) => {
+            const msd = data.advertisement.manufacturerData
+            expect(msd.type).toBe('iBS08')
+            expect(msd.temperature).toBe(21.5)
+            expect(msd.temperatureEnv).toBe(22.4)
+            expect(msd.events.din).toBe(true)
+        })
+    })
+
+})
