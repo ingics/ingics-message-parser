757411a6eaf09a3e079238ff4617249af7688ae9
Tim Chao <tim.chao@ingics.com>
2023-06-07 11:42:38 +0800

    fix: fix wrong test case for ibs08
    Signed-off-by: Tim Chao <tim.chao@ingics.com>

--- __test__/ibs08.test.js
+++ __test__/ibs08.test.js
@@ -2,14 +2,14 @@ const parser = require('..')
 
 describe('various ibs08 payload test', () => {
 
-    it('iBS03T', () => {
+    it('iBS08', () => {
         const message = '$GPRP,F83060BC466E,98F4AB891854,-82,02010612FF2C0883BC380120C0086608000048080400'
         parser.parseMessage(message, (data) => {
             const msd = data.advertisement.manufacturerData
             expect(msd.type).toBe('iBS08')
             expect(msd.temperature).toBe(21.5)
             expect(msd.temperatureEnv).toBe(22.4)
-            expect(msd.events.ir).toBe(true)
+            expect(msd.events.detect).toBe(true)
         })
     })
 
