11774e0afd62eb51f6e9d4acad06e5c7b3e333f1
Tim Chao <tim.chao@ingics.com>
2024-07-15 16:44:04 +0800

    feat: added iBS09 support
    1. Rename iBS08PIR/PS/R to iBS09
    2. Added iBS09LX support
    
    Signed-off-by: Tim Chao <tim.chao@ingics.com>

--- __test__/ibs08.test.js
+++ __test__/ibs08.test.js
@@ -23,19 +23,19 @@ describe('various ibs08 payload test', () => {
         expect(msd.user).toBe(0)
     })
 
-    it('iBS08R', () => {
+    it('iBS09R', () => {
         const payload = '02010612FF2C0883BC280100AAAA7200000042090000'
         let msd = parser.parsePayload(payload).manufacturerData
-        expect(msd.type).toBe('iBS08R')
+        expect(msd.type).toBe('iBS09R')
         expect(msd.battery).toBe(2.96)
         expect(msd.range).toBe(114)
         expect(msd.user).toBe(0)
     })
 
-    it('iBS08PS', () => {
+    it('iBS09PS', () => {
         let payload = '02010612FF2C0883BC1E012021071E00000043010100'
         let msd = parser.parsePayload(payload).manufacturerData
-        expect(msd.type).toBe('iBS08PS')
+        expect(msd.type).toBe('iBS09PS')
         expect(msd.battery).toBe(2.86)
         expect(msd.value).toBe(1825)
         expect(msd.counter).toBe(30)
@@ -43,7 +43,7 @@ describe('various ibs08 payload test', () => {
         expect(msd.events.detect).toBe(true)
         payload = '02010612FF2C0883BC170100B2FF1800000043010100'
         msd = parser.parsePayload(payload).manufacturerData
-        expect(msd.type).toBe('iBS08PS')
+        expect(msd.type).toBe('iBS09PS')
         expect(msd.battery).toBe(2.79)
         expect(msd.value).toBe(-78)
         expect(msd.counter).toBe(24)
@@ -54,9 +54,18 @@ describe('various ibs08 payload test', () => {
     it('iBS08PIR', () => {
         const payload = '02010612FF2C0883BC4A0110AAAAFFFF000044040000'
         let msd = parser.parsePayload(payload).manufacturerData
-        expect(msd.type).toBe('iBS08PIR')
+        expect(msd.type).toBe('iBS09PIR')
         expect(msd.battery).toBe(3.3)
         expect(msd.events.pir).toBe(true)
+        expect(msd.user).toBe(0)
+    })
+
+    it('iBS09LX', () => {
+        const payload = '02010612FF2C0883BC280100AAAA7200000045090000'
+        let msd = parser.parsePayload(payload).manufacturerData
+        expect(msd.type).toBe('iBS09LX')
+        expect(msd.battery).toBe(2.96)
+        expect(msd.lux).toBe(114)
         expect(msd.user).toBe(0)
     })
 })
