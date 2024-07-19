a5cc68264f1124f338e572692192bf38960f9cad
Tim Chao <tim.chao@ingics.com>
2024-07-19 15:35:06 +0800

    feat: ibs08/ibs09 change to use long term payload
    Use product id 0xBC87 for iBS07 and iWS01.
    
    Signed-off-by: Tim Chao <tim.chao@ingics.com>

--- __test__/ibs08.test.js
+++ __test__/ibs08.test.js
@@ -14,58 +14,51 @@ describe('various ibs08 payload test', () => {
     })
 
     it('iBS08T', () => {
-        const payload = '02010612FF2C0883BC4C0100CF080B02000041010C00'
+        const payload = '02010618FF2C0887BC2C01000B0BA301000000000000000041000000'
         let msd = parser.parsePayload(payload).manufacturerData        
         expect(msd.type).toBe('iBS08T')
-        expect(msd.battery).toBe(3.32)
-        expect(msd.temperature).toBe(22.55)
-        expect(msd.humidity).toBe(52.3)
-        expect(msd.user).toBe(0)
+        expect(msd.battery).toBe(3)
+        expect(msd.temperature).toBe(28.27)
+        expect(msd.humidity).toBe(41.9)
+        expect(msd.events.button).toBe(false)
     })
 
     it('iBS09R', () => {
-        const payload = '02010612FF2C0883BC280100AAAA7200000042090000'
+        const payload = '02010618FF2C0887BC470100AAAA7400000000000000000042100000'
         let msd = parser.parsePayload(payload).manufacturerData
         expect(msd.type).toBe('iBS09R')
-        expect(msd.battery).toBe(2.96)
-        expect(msd.range).toBe(114)
-        expect(msd.user).toBe(0)
+        expect(msd.battery).toBe(3.27)
+        expect(msd.range).toBe(116)
     })
 
     it('iBS09PS', () => {
-        let payload = '02010612FF2C0883BC1E012021071E00000043010100'
+        let payload = '02010618FF2C0887BC470120AAAA0100000000000000000043100000'
         let msd = parser.parsePayload(payload).manufacturerData
         expect(msd.type).toBe('iBS09PS')
-        expect(msd.battery).toBe(2.86)
-        expect(msd.value).toBe(1825)
-        expect(msd.counter).toBe(30)
-        expect(msd.user).toBe(0)
+        expect(msd.battery).toBe(3.27)
+        expect(msd.counter).toBe(1)
         expect(msd.events.detect).toBe(true)
-        payload = '02010612FF2C0883BC170100B2FF1800000043010100'
+        payload = '02010618FF2C0887BC470100AAAA0000000000000000000043100000'
         msd = parser.parsePayload(payload).manufacturerData
         expect(msd.type).toBe('iBS09PS')
-        expect(msd.battery).toBe(2.79)
-        expect(msd.value).toBe(-78)
-        expect(msd.counter).toBe(24)
-        expect(msd.user).toBe(0)
+        expect(msd.battery).toBe(3.27)
+        expect(msd.counter).toBe(0)
         expect(msd.events.detect).toBe(false)
     })
 
     it('iBS08PIR', () => {
-        const payload = '02010612FF2C0883BC4A0110AAAAFFFF000044040000'
+        const payload = '02010618FF2C0887BC470110AAAAFFFF000000000000000044100000'
         let msd = parser.parsePayload(payload).manufacturerData
         expect(msd.type).toBe('iBS09PIR')
-        expect(msd.battery).toBe(3.3)
+        expect(msd.battery).toBe(3.27)
         expect(msd.events.pir).toBe(true)
-        expect(msd.user).toBe(0)
     })
 
     it('iBS09LX', () => {
-        const payload = '02010612FF2C0883BC280100AAAA7200000045090000'
+        const payload = '02010618FF2C0887BC470110AAAAFFFF010200000000000045100000'
         let msd = parser.parsePayload(payload).manufacturerData
         expect(msd.type).toBe('iBS09LX')
-        expect(msd.battery).toBe(2.96)
-        expect(msd.lux).toBe(114)
-        expect(msd.user).toBe(0)
+        expect(msd.battery).toBe(3.27)
+        expect(msd.lux).toBe(513)
     })
 })
--- __test__/iws01.test.js
+++ __test__/iws01.test.js
@@ -2,19 +2,6 @@ const parser = require('..')
 
 describe('various iWS01 payload test', () => {
 
-    // stay here for backward compatibility only
-    it('iWS01 (Deprecated format)', () => {
-        const message = '$GPRP,EAC653D3AA8D,CCB97E7361A4,-44,02010612FF2C0883BC4A0100A10A3100000039000000'
-        parser.parseMessage(message, (data) => {
-            const ad = data.advertisement
-            const msd = ad.manufacturerData
-            expect(msd.type).toBe('iWS01')
-            expect(msd.battery).toBe(3.3)
-            expect(msd.temperature).toBe(27.21)
-            expect(msd.humidity).toBe(4.9)
-        })
-    })
-
     it('iWS01', () => {
         const message = '$GPRP,EAC653D3AA8D,CCB97E7361A4,-44,02010618FF2C0887BC4A0100A10A3100000000000000000039000000'
         parser.parseMessage(message, (data) => {
