06c2b8feffad0c60e2d6d04f235d4140f58586dd
Tim Chao <tim.chao@ingics.com>
2024-12-26 14:16:23 +0800

    feat: Support new iBS08/iBS09 payload format (BC88)
    Signed-off-by: Tim Chao <tim.chao@ingics.com>

--- __test__/ibs08.test.js
+++ __test__/ibs08.test.js
@@ -2,7 +2,7 @@ const parser = require('..')
 
 describe('various ibs08 payload test', () => {
 
-    it('iBS08', () => {
+    it('iBS08 (deprecated)', () => {
         const message = '$GPRP,F83060BC466E,98F4AB891854,-82,02010612FF2C0883BC380120C0086608000048080400'
         parser.parseMessage(message, (data) => {
             const msd = data.advertisement.manufacturerData
@@ -13,7 +13,7 @@ describe('various ibs08 payload test', () => {
         })
     })
 
-    it('iBS08T', () => {
+    it('iBS08T (deprecated)', () => {
         let payload = '02010618FF2C0887BC2C01000B0BA301000000000000000041000000'
         let msd = parser.parsePayload(payload).manufacturerData        
         expect(msd.type).toBe('iBS08T')
@@ -30,7 +30,7 @@ describe('various ibs08 payload test', () => {
         expect(msd.events.button).toBe(true)
     })
 
-    it('iBS09R', () => {
+    it('iBS09R (deprecated)', () => {
         const payload = '02010618FF2C0887BC470100AAAA7400000000000000000042100000'
         let msd = parser.parsePayload(payload).manufacturerData
         expect(msd.type).toBe('iBS09R')
@@ -38,7 +38,7 @@ describe('various ibs08 payload test', () => {
         expect(msd.range).toBe(116)
     })
 
-    it('iBS09PS', () => {
+    it('iBS09PS (deprecated)', () => {
         let payload = '02010618FF2C0887BC470120AAAA0100000000000000000043100000'
         let msd = parser.parsePayload(payload).manufacturerData
         expect(msd.type).toBe('iBS09PS')
@@ -53,7 +53,7 @@ describe('various ibs08 payload test', () => {
         expect(msd.events.detect).toBe(false)
     })
 
-    it('iBS08PIR', () => {
+    it('iBS08PIR (deprecated)', () => {
         let payload = '02010618FF2C0887BC470110AAAAFFFF000000000000000044100000'
         let msd = parser.parsePayload(payload).manufacturerData
         expect(msd.type).toBe('iBS09PIR')
@@ -66,7 +66,7 @@ describe('various ibs08 payload test', () => {
         expect(msd.events.pir).toBe(false)
     })
 
-    it('iBS08TL', () => {
+    it('iBS08TL (deprecated)', () => {
         let payload = '02010618FF2C0887BC4701010B0BA301010200000000000045100000'
         let msd = parser.parsePayload(payload).manufacturerData
         expect(msd.type).toBe('iBS08TL')
@@ -76,6 +76,79 @@ describe('various ibs08 payload test', () => {
         expect(msd.lux).toBe(513)
         expect(msd.events.button).toBe(true)
         payload = '02010618FF2C0887BC2001006E281300B90700000000000045100000'
+        msd = parser.parsePayload(payload).manufacturerData
+        expect(msd.type).toBe('iBS08TL')
+        expect(msd.battery).toBe(2.88)
+        expect(msd.temperature).toBe(103.5)
+        expect(msd.humidity).toBe(1.9)
+        expect(msd.lux).toBe(1977)
+        expect(msd.events.button).toBe(false)
+    })
+
+    it('iBS08T', () => {
+        let payload = '0201061AFF2C0888BC2C01000B0BA3010000000000000000000041000000'
+        let msd = parser.parsePayload(payload).manufacturerData        
+        expect(msd.type).toBe('iBS08T')
+        expect(msd.battery).toBe(3)
+        expect(msd.temperature).toBe(28.27)
+        expect(msd.humidity).toBe(41.9)
+        expect(msd.events.button).toBe(false)
+        payload = '0201061AFF2C0888BCE600016E2813000000000000000000000041000000'
+        msd = parser.parsePayload(payload).manufacturerData
+        expect(msd.type).toBe('iBS08T')
+        expect(msd.battery).toBe(2.3)
+        expect(msd.temperature).toBe(103.5)
+        expect(msd.humidity).toBe(1.9)
+        expect(msd.events.button).toBe(true)
+    })
+
+    it('iBS09R', () => {
+        const payload = '0201061AFF2C0888BC470100AAAA74000000000000000000000042100000'
+        let msd = parser.parsePayload(payload).manufacturerData
+        expect(msd.type).toBe('iBS09R')
+        expect(msd.battery).toBe(3.27)
+        expect(msd.range).toBe(116)
+    })
+
+    it('iBS09PS', () => {
+        let payload = '0201061AFF2C0888BC470120AAAA01000000000000000000000043100000'
+        console.log(parser.parsePayload(payload))
+        let msd = parser.parsePayload(payload).manufacturerData
+        expect(msd.type).toBe('iBS09PS')
+        expect(msd.battery).toBe(3.27)
+        expect(msd.counter).toBe(1)
+        expect(msd.events.detect).toBe(true)
+        payload = '0201061AFF2C0888BC470100AAAA00000000000000000000000043100000'
+        msd = parser.parsePayload(payload).manufacturerData
+        expect(msd.type).toBe('iBS09PS')
+        expect(msd.battery).toBe(3.27)
+        expect(msd.counter).toBe(0)
+        expect(msd.events.detect).toBe(false)
+    })
+
+    it('iBS08PIR', () => {
+        let payload = '0201061AFF2C0888BC470110AAAAFFFF0000000000000000000044100000'
+        let msd = parser.parsePayload(payload).manufacturerData
+        expect(msd.type).toBe('iBS09PIR')
+        expect(msd.battery).toBe(3.27)
+        expect(msd.events.pir).toBe(true)
+        payload = '0201061AFF2C0888BCFA0000AAAAFFFF0000000000000000000044100000'
+        msd = parser.parsePayload(payload).manufacturerData
+        expect(msd.type).toBe('iBS09PIR')
+        expect(msd.battery).toBe(2.50)
+        expect(msd.events.pir).toBe(false)
+    })
+
+    it('iBS08TL', () => {
+        let payload = '0201061AFF2C0888BC4701010B0BA3010102000000000000000045100000'
+        let msd = parser.parsePayload(payload).manufacturerData
+        expect(msd.type).toBe('iBS08TL')
+        expect(msd.battery).toBe(3.27)
+        expect(msd.temperature).toBe(28.27)
+        expect(msd.humidity).toBe(41.9)
+        expect(msd.lux).toBe(513)
+        expect(msd.events.button).toBe(true)
+        payload = '0201061AFF2C0888BC2001006E281300B907000000000000000045100000'
         msd = parser.parsePayload(payload).manufacturerData
         expect(msd.type).toBe('iBS08TL')
         expect(msd.battery).toBe(2.88)
